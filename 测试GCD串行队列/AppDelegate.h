8ec10d7e024cda349c382adbd43da3ed1456bc4b
caideyong <caideyong@foxmail.com>
2016-06-24 09:12:06 +0800

    Initial Commit

---
+++ 测试GCD串行队列/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试GCD串行队列
+//
+//  Created by Apple on 16/6/24.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试GCD串行队列/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试GCD串行队列
+//
+//  Created by Apple on 16/6/24.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
